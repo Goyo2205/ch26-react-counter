f1999934e81c1c8ac0abdedf944c352540897eaf
goyo2205 <jgregorio.ef@gmail.com>
2023-05-09 16:47:31 -0600

    se usan otros archivos y propTypes

---
+++ src/components/paragraphLink/ParagraphLink.jsx
@@ -0,0 +1,40 @@
+//importar hojas de estilo
+import '../../../src/styles/paragraphLink/paragraph-link.css'
+
+import PropTypes from 'prop-types';
+
+
+const ParagraphLink = ( {color,href,children} ) => {
+    const myParagraph = (
+        <>
+            <a style={{color: color}} 
+                href={href} target="_blank">
+                <p className='content'>{children}</p>
+            </a>
+        </>
+    );
+    return myParagraph;
+} ;
+
+
+ParagraphLink.propTypes = {
+    href : PropTypes.string.isRequired
+}
+
+
+//Props (properties): Comunican los componentes padres con los componnentes hijos
+/* const ParagraphLink = (props) => {
+    console.log(props);//clave valor, href
+
+    const myParagraph = (
+        <>
+            <a style={{textDecoration: 'none', color: props.color}} 
+                href={props.href} target="_blank">
+                <p>{props.children}</p>
+            </a>
+        </>
+    );
+    return myParagraph;
+} ; */
+
+export default ParagraphLink; // exportacion por default
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Paragraph from './components/paragraphLink/ParagraphLink';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Paragraph href='https://legacy.reactjs.org' pepito='Yo mero caguamero' color ='black'>Enlace a React</Paragraph>
+    <Paragraph href='https://youtube.com' color='red'>Enlace a Youtube</Paragraph>
+    <Paragraph href='https://linkedin.com' color='blue'>Enlance a LinkedIn</Paragraph>
+    <Paragraph href='#'>Enlance a LinkedIn</Paragraph>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* root.render(<h1>HoliCrayoli</h1>) */
+
