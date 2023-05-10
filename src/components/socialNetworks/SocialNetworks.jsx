1f3b6b41b81122f8fb9419c16f5aa345e9177422
goyo2205 <jgregorio.ef@gmail.com>
2023-05-10 10:40:20 -0600

    ci: funcion onClick

--- src/components/socialNetworks/SocialNetworks.jsx
+++ src/components/socialNetworks/SocialNetworks.jsx
@@ -5,8 +5,8 @@ export const SocialNetworks = () => {
   return (
     <>
         <Paragraph href='https://legacy.reactjs.org' pepito='Yo mero caguamero' color ='black'>Link a React</Paragraph>
-        <Paragraph href='https://youtube.com' color='red'>Enlace</Paragraph>
-        <Paragraph href='https://linkedin.com' color='blue'/>
+        <Paragraph href='https://youtube.com' color='red'>Enlace a Youtube</Paragraph>
+        <Paragraph href='https://linkedin.com' color='blue'>Enlace a LinkedIn</Paragraph>
     </>
   )
 }
